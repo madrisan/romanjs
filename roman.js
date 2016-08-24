8ef9d4213b46a327641a28f4324936716736a835
Davide Madrisan <davide.madrisan@gmail.com>
2016-08-24 18:55:06 +0200

    romanjs: add some initial testing code
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- roman.js
+++ roman.js
@@ -6,24 +6,57 @@ var RomanNumber = function(value) {
 };
 
 RomanNumber.prototype = {
+    isDecimal: function() {
+        return +this.value === +this.value;
+    },
 
     toInt: function() {
-        return 'toInt(' + this.value + ')';
+        return this.isDecimal() ? this.value : 'not-implemented';
     },
 
     toString: function() {
-        return 'toString(' + this.value + ')';
+        return this.isDecimal() ? 'not-implemented' : this.value;
     }
 };
 
 
-var romanNumber0 = new RomanNumber();
-var romanNumber0 = new RomanNumber('');
-var romanNumber1 = new RomanNumber('XX');
-var romanNumber2 = new RomanNumber(40);
+// tests
+var testObjs = [
+        null, '', 0, 1, 3, 4, 5,
+        'I', 'III', 'IIII', 'IV', 'V',
+        1968, '1473', 2999, 3000, 10000,
+        'CDXXIX', 'CD1X', 'error', 'MCDLXXXII', 'MCMLXXX',
+        'MMMMCMXCIX', 'MMMMDMXCIX'
+    ],
 
-console.log(romanNumber1.toInt());  // => 20
-console.log(romanNumber1.toString());  // => 'XX'
+    isEqual = function(str1, str2) {
+        return str1 === str2 ? 'Yes' : 'False';
+    },
 
-console.log(romanNumber2.toInt());  // => 40
-console.log(romanNumber2.toString());  // => 'XL'
+    testRoman = function() {
+        var romanNumber1 = new RomanNumber('XX');
+        var romanNumber2 = new RomanNumber(40);
+
+        console.log(romanNumber1.toInt());  // => 20
+        console.log(romanNumber1.toString());  // => 'XX'
+        console.log(romanNumber2.toInt());  // => 40
+        console.log(romanNumber2.toString());  // => 'XL'
+
+        testObjs.forEach(function(str) {
+            try {
+                num = new RomanNumber(str);
+                if (num.isDecimal()) {
+                    var d = num.toInt();
+                    console.log('Is "%s" equal to "%s"?', str, d, isEqual(str, d));
+                }
+                else {
+                    console.log('%s is not an arabic number, not yet implemented', str);
+                }
+            }
+            catch(e) {
+                console.log(e.name + ': ' + e.message + ' (%s)', str);
+            }
+        });
+    };
+
+testRoman();
