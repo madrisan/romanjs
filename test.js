612ce33ada5ff897dcee26deec0b1695a7a51983
Davide Madrisan <davide.madrisan@gmail.com>
2016-08-25 23:40:11 +0200

    romanjs: move the tests to the (gulp-)mocha framework
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- test.js
+++ test.js
@@ -1,41 +1,88 @@
-var RomanNumber = require('./roman');
+var assert = require('assert')
+  , RomanNumber;
 
-var testObjs = [
-        null, '', 0, 1, 3, 4, 5,
-        'I', 'III', 'IIII', 'IV', 'V',
-        1968, '1473', 2999, 3000, 10000,
-        'CDXXIX', 'CD1X', 'error', 'MCDLXXXII', 'MCMLXXX',
-        'MMMMCMXCIX', 'MMMMDMXCIX'
-    ],
+describe('Tests for the class RomanNumber()', function() {
+    //var testObjs = [
+    //    1, 3, 4, 5,
+    //    'I', 'III', 'IIII', 'IV', 'V',
+    //    1968, 2999, 3000,
+    //    'CDXXIX', 'MCDLXXXII', 'MCMLXXX',
+    //],
 
-    isEqual = function(str1, str2) {
-        return str1 === str2 ? 'Yes' : 'False';
-    },
+    //isEqual = function(str1, str2) {
+    //    return str1 === str2 ? 'Yes' : 'False';
+    //},
 
-    testRoman = function() {
-        var romanNumber1 = new RomanNumber('XX');
-        var romanNumber2 = new RomanNumber(40);
-        var romanNumber3 = new RomanNumber(1995);
+    before(function() {
+        RomanNumber = require('./roman');
+    });
 
-        console.log("XX is equal to 20: " + isEqual(20, romanNumber1.toInt()));
-        console.log("XX is equal to XX: " + isEqual('XX', romanNumber1.toString()));
-        console.log("40 is equal to XL: " + isEqual('XL', romanNumber2.toString()));
-        console.log("1995 is equal to MCMXCV: " + isEqual('MCMXCV', romanNumber3.toString()));
+    it('an exception is thrown if the input is null', function(done) {
+        assert.throws(() => { new RomanNumber(null); }, Error);
+        done();
+    });
 
-        testObjs.forEach(function(str) {
-            try {
-                var num = new RomanNumber(str);
-                if (num.isDecimal()) {
-                    console.log('%s is an arabic number, in roman notation: %s', str, num.toString());
-                }
-                else if (num.isRoman()) {
-                    console.log('%s is a roman number, in arabic notation: %s', str, num.toInt());
-                }
-            }
-            catch(e) {
-                console.log(e.name + ': ' + e.message + ' (%s)', str);
-            }
+    it('an exception is thrown if the input is an empty string', function(done) {
+        assert.throws(() => { new RomanNumber(''); }, Error);
+        done();
+    });
+
+    it('an exception is thrown if the input is a number out of range', function(done) {
+        assert.throws(() => { new RomanNumber(0); }, Error);
+        assert.throws(() => { new RomanNumber(10000); }, Error);
+        done();
+    });
+
+    it('an exception is thrown if the input is a number in string format', function(done) {
+        assert.throws(() => { new RomanNumber('1473'); }, Error);
+        done();
+    });
+
+    it('an exception is thrown if the input string is not a roman', function(done) {
+        assert.throws(() => { new RomanNumber('CD1X'); }, Error);
+        assert.throws(() => { new RomanNumber('error'); }, Error);
+        done();
+    });
+
+    it('an exception is thrown if a symbol appears more than three consecutive times', function(done) {
+        var testObjs = [
+            'IIII', 'MMMMCMXCIX', 'MMMMDMXCIX'
+        ];
+        testObjs.forEach(function(number) {
+            assert.throws(() => { new RomanNumber(number); }, Error, 'exception should be raised for ' + number);
         });
-    };
+        done();
+    });
 
-testRoman();
+    it('valid inputs are accepted', function(done) {
+        var testObjs = [
+            1, 3, 4, 5,
+            'I', 'III', 'IV', 'V',
+            1968, 2999, 3000,
+            'CDXXIX', 'MCDLXXXII', 'MCMLXXX'
+        ];
+        testObjs.forEach(function(number) {
+            assert.doesNotThrow(() => { new RomanNumber(number); }, 'got unwanted exception for ' + number);
+        });
+        done();
+    });
+
+    it('roman to arabic conversion (XX)', function(done) {
+        var romanNumber = new RomanNumber('XX');
+        assert.equal(romanNumber.toInt(), '20');
+        done();
+    });
+
+    it('arabic to roman conversion (40)', function(done) {
+        var romanNumber = new RomanNumber(40);
+        assert.equal(romanNumber.toString(), 'XL');
+        done();
+    });
+
+    it('arabic to roman conversion (1995)', function(done) {
+        var romanNumber = new RomanNumber(1995);
+        assert.equal(romanNumber.toString(), 'MCMXCV');
+        done();
+    });
+
+});
