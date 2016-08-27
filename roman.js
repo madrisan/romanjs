5ef1652547d2e4f446397088e8b4ef972e9255a9
Davide Madrisan <davide.madrisan@gmail.com>
2016-08-27 11:12:07 +0200

    roman: make "symtable" read-only
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- roman.js
+++ roman.js
@@ -19,21 +19,24 @@ var RomanNumber = function(value) {
     obj.decimal = null;
     obj.roman = null;
 
-    obj.symtable = {
-        'M'  : 1000,
-        'CM' :  900,
-        'D'  :  500,
-        'CD' :  400,
-        'C'  :  100,
-        'XC' :   90,
-        'L'  :   50,
-        'XL' :   40,
-        'X'  :   10,
-        'IX' :    9,
-        'V'  :    5,
-        'IV' :    4,
-        'I'  :    1
-    };
+    Object.defineProperty(obj, "symtable", {
+        value: {
+            'M'  : 1000,
+            'CM' :  900,
+            'D'  :  500,
+            'CD' :  400,
+            'C'  :  100,
+            'XC' :   90,
+            'L'  :   50,
+            'XL' :   40,
+            'X'  :   10,
+            'IX' :    9,
+            'V'  :    5,
+            'IV' :    4,
+            'I'  :    1
+        },
+        writable: false
+    });
 
     // validate the input
     obj.validateInput(value);
@@ -111,7 +114,6 @@ RomanNumber.prototype = {
                  return false;
             };
         }
-
         return true;
     },
 
